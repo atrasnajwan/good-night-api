4a9d4b63f9525ce01ed34ce654b7a419af94f7d8
atrasnajwan <atrasnajwan@gmail.com>
2025-04-16 15:17:13 +0700

    set instance variable current_user from jwt login token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::API
     include Pagy::Backend
+    attr_reader :current_user
 
     rescue_from ActionController::ParameterMissing, with: :handle_missing_params
     rescue_from ActiveRecord::NotNullViolation, with: :handle_not_null_violation
 
-    # handle if got wrong page
+    # handle if got wrong page/out of scope page
     rescue_from Pagy::OverflowError do
       render json: { error: 'Page out of range' }, status: :bad_request
     end
@@ -33,5 +34,19 @@ class ApplicationController < ActionController::API
           message: "NULL value not allowed",
           error: exception.message
         }, status: :unprocessable_entity
+    end
+
+    def authenticate_user
+      header = request.headers['Authorization']
+      return render json: { error: 'Missing token' }, status: :unauthorized unless header # check HTTP header
+  
+      token = header.split(' ').last
+  
+      begin
+        decoded = JWT.decode(token, JWT_SECRET_KEY)[0] # decode JWT token
+        @current_user = User.find(decoded["user_id"])
+      rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+        render json: { error: 'Invalid or expired token' }, status: :unauthorized
+      end
     end
 end
