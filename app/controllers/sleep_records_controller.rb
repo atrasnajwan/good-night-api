6689886746f1d252fab3236357bb1df87a4b0c90
atrasnajwan <atrasnajwan@gmail.com>
2025-04-16 13:05:38 +0700

    handle clock_in method - can't clock in if not clocked out before

--- app/controllers/sleep_records_controller.rb
+++ app/controllers/sleep_records_controller.rb
@@ -18,4 +18,37 @@ class SleepRecordsController < ApplicationController
         render json: sleep_records
     end
 
+    def clock_in
+        current_sleep = SleepRecord.find_by(user_id: clock_in_params[:user_id], clocked_out_at: nil)
+        
+        # can't clock in if not clocked out before
+        if current_sleep.present?
+            render json: {
+                message: "Already clocked in",
+                error: "You must clock out before clocking in again"
+              }, status: :unprocessable_entity
+        else
+            new_sleep_record = SleepRecord.new(clock_in_params)
+    
+            if new_sleep_record.save
+                render json: new_sleep_record, status: :created
+            else
+                render json: { errors: new_sleep_record.errors }, status: :unprocessable_entity
+            end
+        end
+    end
+
+    private
+    
+    # Example
+    # {
+    #     "sleep_record": { 
+    #         "user_id": 99
+    #     }
+    # }
+    def clock_in_params
+        params.required(:sleep_record)
+              .permit(:user_id)
+              .merge(clocked_in_at: Time.now) # merge current time to param clocked_in_at
+    end
 end
