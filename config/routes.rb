2d5626b77ec3117edca3871b806f8bd91162c48f
atrasnajwan <atrasnajwan@gmail.com>
2025-04-16 12:48:46 +0700

    add new sleep_records endpoint to the routes - GET /users/:user_id/sleep_records - GET  /sleep_records - POST /sleep_records - PATCH /sleep_records/:id/clock_out

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # /users
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :create] do
+    # GET /users/:user_id/sleep_records
+    get 'sleep_records', to: 'sleep_records#user_sleep_records' 
+  end
+
+  # /sleep_records
+  resources :sleep_records, only: [:index] do
+    # /sleep_records/:id/clock_out
+    patch 'clock_out', on: :member
+  end
+
+  # POST /sleep_records
+  post '/sleep_records', to: 'sleep_records#clock_in'
 end
